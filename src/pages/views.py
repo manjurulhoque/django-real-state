ea9004528ffbf7a181ba05cd4748304610edee12
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-06-11 02:06:29 +0600

    Revise about page layout: implement new hero section, enhance content structure, and improve team display with updated styling and features. Update database file.

--- src/pages/views.py
+++ src/pages/views.py
@@ -10,27 +10,24 @@ from listings.choices import state_choices, bedroom_choices, price_choices
 
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
 
     context = {
-        'listings': listings,
-        'state_choices': state_choices,
-        'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
     }
 
-    return render(request, 'pages/index.html', context)
+    return render(request, "pages/index.html", context)
 
 
 def about(request):
     # Get all realtors
-    realtors = Realtor.objects.order_by('-hire_date')
+    realtors = Realtor.objects.order_by("-hire_date")
 
     # Get MVP
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
-    context = {
-        'realtors': realtors,
-        'mvp_realtors': mvp_realtors
-    }
-    return render(request, 'pages/about.html', context)
+    context = {"realtors": realtors, "mvp_realtors": mvp_realtors}
+    return render(request, "pages/about.html", context)
